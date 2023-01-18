3f0b247b9df65c4f7296511adc4cfc9bc47d344f
Amol Borkar <amolborkar5@gmail.com>
2023-01-18 10:20:22 +0530

    Add utils and update starts

--- backtest.py
+++ backtest.py
@@ -1,121 +1,64 @@
+import time
 import pandas as pd
 import backtrader as bt
-
-
-class VolumeWeightedAveragePrice(bt.Indicator):
-    plotinfo = dict(subplot=False)
-
-    params = (("period", 30),)
-
-    alias = (
-        "VWAP",
-        "VolumeWeightedAveragePrice",
-    )
-    lines = ("VWAP",)
-    plotlines = dict(VWAP=dict(alpha=0.50, linestyle="-.", linewidth=2.0))
-
-    def __init__(self):
-        # Before super to ensure mixins (right-hand side in subclassing)
-        # can see the assignment operation and operate on the line
-        cumvol = bt.ind.SumN(self.data.volume, period=self.p.period)
-        typprice = (
-            (self.data.close + self.data.high + self.data.low) / 3
-        ) * self.data.volume
-        cumtypprice = bt.ind.SumN(typprice, period=self.p.period)
-        self.lines[0] = cumtypprice / cumvol
-
-        super(VolumeWeightedAveragePrice, self).__init__()
+from utils import Utils, Logger
 
 
 class TestStrategy(bt.Strategy):
-    def log(self, txt, dt=None):
-        dt = dt or self.datas[0].datetime.date(0)
-        print("%s, %s" % (dt.isoformat(), txt))
-
     def __init__(self):
-        self.dataclose = self.datas[0].close
-        self.order = None
-
-        price = self.data
-        ema = bt.ind.EMA(period=50)
-        self.rsi = bt.ind.RSI(period=2, safediv=True)
-        self.crossover = bt.ind.CrossOver(price, ema)
-
-    def notify_order(self, order):
-        if order.status in [order.Submitted, order.Accepted]:
-            return
-
-        if order.status in [order.Completed]:
-            if order.isbuy():
-                self.log("BUY EXECUTED, %.2f" % order.executed.price)
-            elif order.issell():
-                self.log("SELL EXECUTED, %.2f" % order.executed.price)
-            self.bar_executed = len(self)
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log("Order Canceled/Margin/Rejected")
-
-        self.order = None
-
-
-class HighVolStrat(bt.Strategy):
-    def log(self, txt, dt=None):
-        dt = dt or self.datas[0].datetime.date(0)
-        print("%s, %s" % (dt.isoformat(), txt))
-
-    def __init__(self):
-        self.dataclose = self.datas[0].close
-        self.order = None
-
-    def notify_order(self, order):
-        if order.status in [order.Submitted, order.Accepted]:
-            return
-
-        if order.status in [order.Completed]:
-            if order.isbuy():
-                self.log("BUY EXECUTED, %.2f" % order.executed.price)
-            elif order.issell():
-                self.log("SELL EXECUTED, %.2f" % order.executed.price)
-            self.bar_executed = len(self)
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log("Order Canceled/Margin/Rejected")
-
-        self.order = None
+        self.rsi = bt.ind.RSI(plot=False)
+        self.price = self.data0.close
+        self.scnt = self.fcnt = 0
 
     def next(self):
-        self.log("Close, %.2f" % self.dataclose[0])
-        if self.order:
-            return
-
         if not self.position:
             if (
-                self.dataclose[0] > self.dataclose[-1]
-                and self.dataclose[-1] > self.dataclose[-2]
-                and self.dataclose[-2] > self.dataclose[-3]
+                self.rsi > 65
+                and self.price[-2] > self.price[-1]
+                and self.price[-1] > self.price[0]
             ):
-                self.enter_point = len(self)
-                self.log("BUY CREATE, %.2f" % self.dataclose[0])
-                self.order = self.buy()
+                self.bp = self.price[0]
+                print(f"Sell: {self.bp}")
+                self.sell()
         else:
-            if len(self) > (self.enter_point + 10):
-                self.log("SELL CREATE, %.2f" % self.dataclose[0])
-                self.order = self.sell()
+            sl = self.bp * 0.004 + self.bp
+            tp = self.bp - self.bp * 0.012
+            if self.price >= sl:
+                print(f"Stop Loss: {self.price[0]}")
+                self.fcnt += 1
+                self.buy()
+
+            if self.price <= tp:
+                print(f"Take Profit: {self.price[0]}")
+                self.scnt += 1
+                self.buy()
+
+    def stop(self):
+        print(
+            f"S: {self.scnt}, F: {self.fcnt}, Win%: {self.scnt / (self.scnt + self.fcnt) * 100}"
+        )
 
 
 if __name__ == "__main__":
+    stock = "TCS"
     df = pd.read_csv(
-        "hist_data/TCS_data.csv", delimiter=",", index_col="datetime", parse_dates=True
+        f"hist_data/{stock}_data.csv",
+        delimiter=",",
+        index_col="datetime",
+        parse_dates=True,
     )
 
     # Get last year's data
-    df = df["2021-01-01 07:00:00":"2023-01-17 07:00:00"]
+    df = df["2023-01-01 07:00:00":"2023-01-17 07:00:00"]
 
     cerebro = bt.Cerebro()
     data = bt.feeds.PandasData(
-        name="TCS 5 Min", dataname=df, timeframe=bt.TimeFrame.Minutes
+        name=f"{stock} 5 Min", dataname=df, timeframe=bt.TimeFrame.Minutes
     )
     cerebro.adddata(data)
     cerebro.broker.set_cash(10000)
-    cerebro.addstrategy(HighVolStrat)
+    cerebro.addstrategy(TestStrategy)
+    cerebro.addsizer(bt.sizers.PercentSizer, percents=90)
     print(f"Starting Portfolio Value: {cerebro.broker.getvalue()}")
     cerebro.run()
     print(f"Final Portfolio Value: {cerebro.broker.getvalue()}")
---
+++ utils.py
@@ -0,0 +1,49 @@
+import backtrader as bt
+
+
+class VWAP(bt.Indicator):
+    plotinfo = dict(subplot=False)
+    params = (("period", 30),)
+    alias = (
+        "VWAP",
+        "VolumeWeightedAveragePrice",
+    )
+    lines = ("VWAP",)
+    plotlines = dict(VWAP=dict(alpha=0.50, linestyle="-.", linewidth=2.0))
+
+    def __init__(self):
+        cumvol = bt.ind.SumN(self.data.volume, period=self.p.period)
+        typprice = (
+            (self.data.close + self.data.high + self.data.low) / 3
+        ) * self.data.volume
+        cumtypprice = bt.ind.SumN(typprice, period=self.p.period)
+        self.lines[0] = cumtypprice / cumvol
+        super(VolumeWeightedAveragePrice, self).__init__()
+
+
+class Logger:
+    def __init__(self, datas) -> None:
+        self.datas = datas
+
+    def log(self, txt):
+        dt = self.datas[0].datetime.date(0)
+        print(f"{dt.isoformat()}, {txt}")
+
+
+class Utils:
+    def __init__(self):
+        pass
+
+    def notify_order(self, obj, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                obj.log(f"BUY EXECUTED, {order.executed.price}")
+            elif order.issell():
+                obj.log(f"SELL EXECUTED, {order.executed.price}")
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            obj.log("Order Canceled/Margin/Rejected")
+
+        obj.order = None

aafe01f4e895bc4e91253da5841396555709527a
Borkar <amborkar@deloitte.com>
2023-01-18 12:29:13 +0530

    code cleanup

--- backtest.py
+++ backtest.py
@@ -3,76 +3,7 @@ import pytz
 import pandas as pd
 import backtrader as bt
 from datetime import datetime, time
-from utils import Utils, Logger
-
-
-class EMAStrat(bt.Strategy):
-    def __init__(self):
-        ema1 = bt.ind.EMA(period=5)
-        self.ema2 = bt.ind.EMA(period=10)
-        self.crossover = bt.ind.CrossOver(ema1, self.ema2, plot=False)
-        self.price = self.data0.close
-        self.scnt = self.fcnt = 0
-
-    def next(self):
-        if not self.position:
-            if self.price[0] > self.ema2[0] and self.crossover > 0:
-                self.bp = self.price[0]
-                print(f"Buy: {self.bp}")
-                self.buy()
-        else:
-            sl = self.bp - 0.004 * self.bp
-            tp = self.bp + 0.012 * self.bp
-
-            if self.price <= sl:
-                print(f"Stop Loss: {self.price[0]}")
-                self.fcnt += 1
-                self.sell()
-
-            if self.price >= tp:
-                print(f"Take Profit: {self.price[0]}")
-                self.scnt += 1
-                self.sell()
-
-    def stop(self):
-        print(
-            f"S: {self.scnt}, F: {self.fcnt}, Win%: {self.scnt / (self.scnt + self.fcnt) * 100}"
-        )
-
-
-class TestStrategy(bt.Strategy):
-    def __init__(self):
-        self.rsi = bt.ind.RSI(plot=False)
-        self.price = self.data0.close
-        self.scnt = self.fcnt = 0
-
-    def next(self):
-        if not self.position:
-            if (
-                self.rsi > 65
-                and self.price[-2] > self.price[-1]
-                and self.price[-1] > self.price[0]
-            ):
-                self.bp = self.price[0]
-                print(f"Sell: {self.bp}")
-                self.sell()
-        else:
-            sl = self.bp * 0.004 + self.bp
-            tp = self.bp - self.bp * 0.012
-            if self.price >= sl:
-                print(f"Stop Loss: {self.price[0]}")
-                self.fcnt += 1
-                self.buy()
-
-            if self.price <= tp:
-                print(f"Take Profit: {self.price[0]}")
-                self.scnt += 1
-                self.buy()
-
-    def stop(self):
-        print(
-            f"S: {self.scnt}, F: {self.fcnt}, Win%: {self.scnt / (self.scnt + self.fcnt) * 100}"
-        )
+from strategies import EMAStrat
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import collect_data
 if __name__ == "__main__":
     breeze = connect.ICICIConnector().connect()
     dc = collect_data.DataCollector(breeze)
-    stocks = ["HDFC", "ITC", "TCS", "BHARTIARTL"]
+    stocks = ["HDFC", "ITC", "TCS"]
     dc.get_stock_hist(stocks, "5minute", "2018-01-01", "2023-01-13")
     for s in stocks:
         dc.dump_to_csv(s)

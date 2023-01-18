b19906a86ab34da04e6a4a016a6c037299c1858d
Amol Borkar <amolborkar5@gmail.com>
2023-01-18 13:23:09 +0530

    reformat

--- backtest.py
+++ backtest.py
@@ -8,30 +8,30 @@ from strategies import EMAStrat
 
 if __name__ == "__main__":
     stock = "ITC"
-    df = pd.read_csv(
-        f"hist_data/{stock}_data.csv",
-        delimiter=",",
-        index_col="datetime",
-        parse_dates=True,
-    )
-    data = bt.feeds.PandasData(
-        name=f"{stock} 5 Min",
-        dataname=df,
-        timeframe=bt.TimeFrame.Minutes,
-        fromdate=datetime(2023, 1, 1),
-        todate=datetime(2023, 1, 17),
-        sessionstart=time(9, 5),
-        sessionend=time(15, 25),
-        tz=pytz.timezone("Asia/Kolkata"),
-    )
 
-    cerebro = bt.Cerebro()
-    cerebro.adddata(data)
-    cerebro.broker.set_cash(10000)
-    cerebro.broker.set_coc(True)
-    cerebro.addstrategy(EMAStrat)
-    cerebro.addsizer(bt.sizers.PercentSizer, percents=90)
-    print(f"Starting Portfolio Value: {cerebro.broker.getvalue()}")
-    cerebro.run()
-    print(f"Final Portfolio Value: {cerebro.broker.getvalue()}")
-    cerebro.plot(style="candlestick")
+    cb = bt.Cerebro()
+    cb.adddata(
+        bt.feeds.PandasData(
+            name=f"{stock} 5 Min",
+            dataname=pd.read_csv(
+                f"hist_data/{stock}_data.csv",
+                delimiter=",",
+                index_col="datetime",
+                parse_dates=True,
+            ),
+            timeframe=bt.TimeFrame.Minutes,
+            fromdate=datetime(2023, 1, 1),
+            todate=datetime(2023, 1, 17),
+            sessionstart=time(9, 5),
+            sessionend=time(15, 25),
+            tz=pytz.timezone("Asia/Kolkata"),
+        )
+    )
+    cb.broker.set_cash(10000)
+    cb.broker.set_coc(True)
+    cb.addstrategy(EMAStrat)
+    cb.addsizer(bt.sizers.PercentSizer, percents=90)
+    print(f"Starting Portfolio Value: {cb.broker.getvalue()}")
+    cb.run()
+    print(f"Final Portfolio Value: {cb.broker.getvalue()}")
+    cb.plot(style="candlestick")

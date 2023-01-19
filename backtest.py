83f1a354c1399ff25c4d63124b503bf58b543004
Amol Borkar <amolborkar5@gmail.com>
2023-01-19 15:02:14 +0530

    add compression

--- backtest.py
+++ backtest.py
@@ -7,26 +7,30 @@ from strategies import EMAStrat
 
 
 if __name__ == "__main__":
-    stock = "ITC"
+    stock = "TCS"
 
     cb = bt.Cerebro()
     cb.adddata(
-        bt.feeds.PandasData(
-            name=f"{stock} 5 Min",
-            dataname=pd.read_csv(
-                f"hist_data/{stock}_data.csv",
-                delimiter=",",
-                index_col="datetime",
-                parse_dates=True,
+        # cb.resampledata(
+            bt.feeds.PandasData(
+                name=f"{stock} 5 Min",
+                dataname=pd.read_csv(
+                    f"hist_data/{stock}_data.csv",
+                    delimiter=",",
+                    index_col="datetime",
+                    parse_dates=True,
+                )["2022-01-01 07:00:00": "2023-01-18 16:00:00"],
+                timeframe=bt.TimeFrame.Minutes,
+                fromdate=datetime(2022, 1, 1),
+                todate=datetime(2023, 1, 18),
+                sessionstart=time(9, 5),
+                sessionend=time(15, 25),
+                tz=pytz.timezone("Asia/Kolkata"),
             ),
-            compression=3,
-            timeframe=bt.TimeFrame.Minutes,
-            fromdate=datetime(2022, 1, 1),
-            todate=datetime(2023, 1, 18),
-            sessionstart=time(9, 5),
-            sessionend=time(15, 25),
-            tz=pytz.timezone("Asia/Kolkata"),
-        )
+        #     timeframe=bt.TimeFrame.Minutes,
+        #     rightedge=False,
+        #     compression=1,
+        # )
     )
     cb.broker.set_cash(10000)
     cb.addstrategy(EMAStrat)
@@ -34,4 +38,4 @@ if __name__ == "__main__":
     print(f"Starting Portfolio Value: {cb.broker.getvalue()}")
     cb.run()
     print(f"Final Portfolio Value: {cb.broker.getvalue()}")
-    # cb.plot(style="candlestick")
+    cb.plot(style="candlestick")

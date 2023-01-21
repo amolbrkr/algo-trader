b7c280a9b2e9c6417ea6fa5fd9a41dbf59d4c79f
Amol Borkar <amolborkar5@gmail.com>
2023-01-21 15:17:23 +0530

    code refactor

--- strategies/strategies.py
+++ strategies/strategies.py
@@ -17,28 +17,30 @@ class BaseStrat(Strategy):
         return (price, sl, tp)
 
 
-class TestStrat(BaseStrat):
+class TestStrat(Strategy):
     def init(self):
         self.price = self.data.Close
         self.rsi = self.I(ta.RSI, self.price, 14)
-        self.ema = self.I(ta.EMA, self.price, 5)
+        self.ema = self.I(ta.EMA, self.price, 6)
         self.ema1 = self.I(ta.EMA, self.price, 15)
 
     def next(self):
-        in_mkt = False
         ltp = self.data.Close[-1]
+        tm = self.data.index[-1]
 
-        if (
-            crossover(self.ema, self.ema1)
-            and self.rsi[-1] >= 40
-            and self.rsi[-1] <= 60
-            and self.rsi[-3] < self.rsi[-2]
-            and self.rsi[-2] < self.rsi[-1]
-        ):
-            self.bp, self.sl, self.tp = self.execute("BUY", ltp)
-            in_mkt = True
+        if not self.position and tm.hour >= 15:
+            return
 
-        if in_mkt and (
-            self.data.index[-1].hour >= 15 or ltp <= self.sl or ltp >= self.tp
-        ):
-            self.sell()
+        if not self.position:
+            if (
+                crossover(self.ema, self.ema1)
+                and self.rsi[-1] >= 40
+                and self.rsi[-1] <= 60
+                and self.rsi[-3] < self.rsi[-2]
+                and self.rsi[-2] < self.rsi[-1]
+            ):
+                self.buy(sl=ltp - ltp * 0.004, tp=ltp + ltp * 0.012)
+        else:
+            if tm.hour >= 15:
+                self.trades[0].close()
+                print(f"Mkt Close: {ltp}")
